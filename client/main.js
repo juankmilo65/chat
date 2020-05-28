db9677d2623dcf7dd6a4856716595f593a1048e9
Juan Camilo Morales Niño <jmoralesn@psl.com.co>
2020-05-28 19:36:42 +0300

    Chreación chat

---
+++ client/main.js
@@ -0,0 +1,32 @@
+var socket = io.connect("http://192.168.43.211:6677", { forceNew: true });
+socket.on("messages", function (data) {
+  console.log(data);
+  render(data);
+});
+
+function render(data) {
+  var html = data
+    .map(function (message, index) {
+      return `
+      <div class="message">
+        <strong>${message.nickName}</strong> dice:
+        <p>${message.text}</p>
+    </div>`;
+    })
+    .join(" ");
+  var divMsgs = document.getElementById("messages");
+
+  divMsgs.innerHTML = html;
+  divMsgs.scrollTo = divMsgs.scrollHeight;
+}
+
+function addMessage(e) {
+  var message = {
+    text: document.getElementById("text").value,
+    nickName: document.getElementById("nickname").value,
+  };
+
+  document.getElementById("nickname").style.display = "none";
+  socket.emit("add-message", message);
+  return false;
+}
